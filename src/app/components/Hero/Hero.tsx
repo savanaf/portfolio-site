91539061d90ab0cd2ac6a496e30c446307878216
Savana Alden <savana@youaremysunshine.design>
2024-10-15 08:52:58 -0600

    Revert to useEffect for copy animation

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from 'react-redux';
 import Menu from "../Menu/Menu";
 
@@ -12,10 +12,14 @@ export default function Hero() {
   const [loading, setLoading] = useState(false);
   const menuOpen = useSelector((state: any) => state.menuOpen)
 
-  const img = new Image();
-  img.src = "/hero-image.jpg";
-  img.onload = () => { setLoading(true); }
-  
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLoading(true)
+    }, 250);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
     <div className={`hero ${menuOpen ? 'menu-open' :''}`}>
       <Menu />
@@ -23,7 +27,7 @@ export default function Hero() {
         <h2>Hello.</h2>
         <h2 className="name">I&apos;m Savana.</h2>
       </div>
-      <div className="hero-image" style={{ backgroundImage: `url(${img.src})`}}></div>
+      <div className="hero-image"></div>
     </div>
   )
 }
